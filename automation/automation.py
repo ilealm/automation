4f1de9a980afc85e1c7b686054b0b78c39c8f683
Iris Leal <iris_leal_m@hotmail.com>
2020-06-04 13:18:17 -0700

    First draft getting the emails

--- automation/automation.py
+++ automation/automation.py
@@ -6,7 +6,7 @@ import re
 # phone numbers should be stored in xxx-yyy-zzzz format.
 
 def is_valid_email(sequence):
-  # re.search(r'([\w\.-]+)@([\w\.-]+)'
+  # https://www.datacamp.com/community/tutorials/python-regular-expression-tutorial
   pattern = r'([\w\.-]+)@([\w\.-]+)'
   if re.match(pattern, sequence):
     return True
@@ -14,45 +14,84 @@ def is_valid_email(sequence):
     return False
 
 def is_valid_phone_number(sequence):
-    # print(sequence)
-    # Format (xxx) yyy-zzzz
-    # Format xxx-yyy-zzzz
-    pattern = r"Cookie"
-    # pattern = r'{3}'
+    # http://regexlib.com/Search.aspx?k=phone
+    # pattern = r"^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$"
+    pattern = r"(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?"
+  
     if re.match(pattern, sequence):
-      print("Match!")
-    else: print("Not a match!")
+        return True
+    else:
+        return False
 
 
-def test():
-    # \w - Lowercase w. Matches any single letter, digit or underscore.
-    # \s - Lowercase s. Matches a single whitespace character like: space, newline, tab, return.
-    # r => This is called a raw string literal. It changes how the string literal is interpreted. Such literals are stored as they appear.
-    # \d - Lowercase d. Matches decimal digit 0-9.
-    # ^ - Caret. Matches a pattern at the start of the string.
-    # $ - Matches a pattern at the end of string.
-    # re.search(r'Number: [0-6]', 'Number: 5').group()[a-zA-Z0-9] - Matches any letter from (a to z) or (A to Z) or (0 to 9). 
-    # Characters that are not within a range can be matched by complementing the set. 
-    # If the first character of the set is ^, all the characters that are not in the set will be matched.
-    # + - Checks for one or more characters to its left.
-    # * - Checks for zero or more characters to its left.
-    # ? - Checks for exactly zero or one character to its left.
-    pattern = r"Cookie"
-    sequence = "Cookie"
-    if re.match(pattern, sequence):
-      print("Match!")
-    else: print("Not a match!")
+def read_file(path_file_to_read):
+    try:
+        with open(path_file_to_read,'r') as my_file:
+                contents = my_file.read()
+        return contents
+    except:
+        print('Error on reading the file.')
 
-    # num = '(425) 996-21-46'
-    num = "111-222-3333"
-    # is_valid_phone_number(sequence)
-    # sequence = 'iris_leal@hotmail.com'
-    sequence = 'name.lastname@domain.net'
-    is_valid_email(sequence)
-    print(is_valid_email(""))
+def extract_information():
+    # read the fil
+    path_file_to_read = "assets/potential-contacts.txt"    
+    # put everyword in a list
+    # content_list = read_file(path_file_to_read).split()
+    content = read_file(path_file_to_read)
+    # print(content_list[0:300])
+    # return
+    # Analyce each word, if is email or phone number, add to set or list
+    # for word in content_list:
+    #     print(word)
+    #     if is_valid_email(word):
+    #         print('email: ', word)
+    #     if is_valid_phone_number(word):
+    #         print('phone: ', word)
+
+    # at the end, sort the lists
+    # store the info on a file
+    
+
+    #'addresses' is a list that stores all the possible match
+    addresses = re.findall(r'[\w\.-]+@[\w\.-]+', content)
+    for address in addresses: 
+        print(address)
+    
 
 
 
-if __name__ == "__main__":
-    test()
-    print('So far, so good')
+if __name__ == "__main__":  
+  extract_information()  
+  # num ="'wrong.092-32-8829She'"
+    # num ='(425)996-2146'
+  # print( is_valid_phone_number(num))
+    # num ='425-996-2146'
+    # print( is_valid_phone_number(num))
+    # num ='958-967-1821x12205'
+    # print( is_valid_phone_number(num))
+    # num ='425996-2146'
+    # print( is_valid_phone_number(num))
+    # num ='001-746-186-9687x72605'
+    # print( is_valid_phone_number(num))
+    # num ='+1-776-470-3636x20415'
+    # print( is_valid_phone_number(num))
+    # num ='368-358-0083'
+    # print( is_valid_phone_number(num))
+    # num ='+1-701-212-0428x18995'
+    # print( is_valid_phone_number(num))
+    # num ='001-746-186-9687x72605'  
+    # print( is_valid_phone_number(num))
+
+    # mail ="hernandezaudrey@hotmail.com"
+    # print(is_valid_email(mail))
+    # mail ="jellison@campbell.com"
+    # print(is_valid_email(mail))
+    # mail ="hsampson@gibson-jackson.com"
+    # print(is_valid_email(mail))
+    # mail ="mary90@yahoo.com"
+    # print(is_valid_email(mail))
+    # mail ="robertsonkristina@cole-cannon.org"
+    # print(is_valid_email(mail))
+    # mail ="dking@mcdonald-adams.com"
+    # print(is_valid_email(mail))
+    
