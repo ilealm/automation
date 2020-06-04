954a6c92592beb34fa7b0c9124655a13bc26c969
Iris Leal <iris_leal_m@hotmail.com>
2020-06-04 15:21:25 -0700

    Ready all core funcionaluty

--- automation/automation.py
+++ automation/automation.py
@@ -1,10 +1,5 @@
 import re
 
-
-# (xxx) yyy-zzzz, yyy-zzzz, xxx-yyy-zzzz, etc.
-# phone numbers with missing area code should presume 206
-# phone numbers should be stored in xxx-yyy-zzzz format.
-
 def read_file(path_file_to_read):
     try:
         with open(path_file_to_read,'r') as my_file:
@@ -13,20 +8,6 @@ def read_file(path_file_to_read):
     except:
         print('Error on reading the file.')
 
-def is_valid_phone_number(sequence):
-  # (\d{10})$)|(^(\d{6}-\d{4})$)|(^\d{3}[\s-]\d{3}[\s-]\d{4}$)|(^\d{3}[\s-]\d{7}$)|(^\(\d{3}\)\s?\d{3}[\s-]\d{4}
-  # this is mine
-  """
-  Regex obtained from : # http://regexlib.com/Search.aspx?k=phone
-    
-  """
-  pattern = r'\b([A-Za-z0-9])*.?([A-Za-z0-9]+)@([A-Za-z0-9])*.((com)|(net)|(org))'
-  if re.match(pattern, sequence):
-    return True
-  else: 
-    return False
-
-
 def is_valid_email(sequence):
   pattern = r'([\w\.-]+)@([\w\.-]+)'
   if re.match(pattern, sequence):
@@ -34,42 +15,48 @@ def is_valid_email(sequence):
   else: 
     return False
 
+    
+def get_phones(content):
+    """
+    Regex: JB Tellez. I Updated "-" to [-.]
+    """
+    pattern = r'[2-9]\d{2}[-.]\d{3}[-.]\d{4}'  # THIS WORKS FOR ddd-ddd-dddd. UNITE THE LAST 2 !
+    return  re.findall(pattern, content) 
 
 
-def extract_information():
+def save_email_list(e_mail_list):
+    path_to_save = 'emails.txt'
+
+    with open(path_to_save, "w+") as f:
+        for element in e_mail_list:
+            f.write(element + '\n')
+
+def save_phone_list(phones_list):
+    path_to_save = 'phone_numbers.txt'
+
+    with open(path_to_save, "w+") as f:
+        for element in phones_list:
+            f.write(element + '\n') 
+
+
+def process_information():
     email_list = []
     phones_list = []
     path_file_to_read = "assets/potential-contacts.txt"    
     
     content = read_file(path_file_to_read)
-    # content_list = read_file(path_file_to_read).split()
-    content_list = content.split()
     
-    for word in content_list:
-        # print(word)
+    for word in content.split(): #content_list: 
         if is_valid_email(word) : email_list.append(word)
     
-    get_phones(content)
-        # if is_valid_phone_number(word) :  
-        #   pass
-            # TODO: remove trash
-            # phones_list.append(word)
-            # print(word)
+    phones_list = get_phones(content)
+ 
+    email_list.sort()
+    phones_list.sort()
 
-    # print(email_list)
-    # print()
-    
-    
-def get_phones(content):
-    # pattern = r'\d{3}\.\d{3}.\d{4}'  # working !!!, but the other get more this gets the ones with .
-    # pattern = r'[2-9]\d{2}-\d{3}-\d{4}'  # THIS WORKS FOR ddd-ddd-dddd. bur the other get more this gets the ones with ..
-    pattern = r'[2-9]\d{2}[-.]\d{3}[-.]\d{4}'  # THIS WORKS FOR ddd-ddd-dddd. UNITE THE LAST 2 !
-    
-    
-    
-    results = re.findall(pattern, content) 
-    print(results)
+    save_email_list(email_list)
+    save_phone_list(phones_list)
 
 
 if __name__ == "__main__":  
-  extract_information()  
+  process_information()  
